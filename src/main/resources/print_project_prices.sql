4c71fa5a0f4990aebd944c3d2a9ad755b76941b0
GRAVDO <42653608+gravdo12345@users.noreply.github.com>
2023-12-12 00:01:38 +0100

    Update print_project_prices.sql

--- src/main/resources/print_project_prices.sql
+++ src/main/resources/print_project_prices.sql
@@ -1,6 +1,15 @@
-SELECT p.NAME, SUM(w.SALARY * TIMESTAMPDIFF(MONTH, p.START_DATE, p.FINISH_DATE)) AS PRICE
-FROM project p
+-- print_project_prices.sql
+
+-- Вивести вартість кожного проєкту
+SELECT
+    p.NAME AS PROJECT_NAME,
+    SUM(w.SALARY * DATEDIFF(pr.FINISH_DATE, pr.START_DATE)) AS PRICE
+FROM
+    project p
 JOIN project_worker pw ON p.ID = pw.PROJECT_ID
 JOIN worker w ON pw.WORKER_ID = w.ID
-GROUP BY p.ID, p.NAME
-ORDER BY PRICE DESC;
+JOIN project pr ON pw.PROJECT_ID = pr.ID
+GROUP BY
+    p.NAME
+ORDER BY
+    PRICE DESC;
