42515e8b500974b9400eee1f5ff0ca9f46e647ab
GRAVDO <42653608+gravdo12345@users.noreply.github.com>
2023-12-14 13:31:51 +0100

    Update find_longest_project.sql

--- src/main/resources/find_longest_project.sql
+++ src/main/resources/find_longest_project.sql
@@ -1,7 +1,8 @@
--- find_longest_project.sql
-
--- Знайти проєкт з найбільшою тривалістю
-SELECT NAME AS PROJECT_NAME, DATEDIFF(FINISH_DATE, START_DATE) AS MONTH_COUNT
-FROM project
-ORDER BY MONTH_COUNT DESC
+SELECT
+    project.NAME AS project_name,
+    DATEDIFF(project.FINISH_DATE, project.START_DATE) AS project_duration
+FROM
+    project
+ORDER BY
+    project_duration DESC
 LIMIT 1;
